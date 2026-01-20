ffca306804e3dfc7df317a40eb60caae74526e5a
johan <johan.persson@iths.se>
2026-01-20 13:20:58 +0100

    skapade en ny class

--- src/main/java/se/iths/johan/webbserver/HelloServlet.java
+++ src/main/java/se/iths/johan/webbserver/HelloServlet.java
@@ -1,4 +1,32 @@
 package se.iths.johan.webbserver;
 
-public class HelloServlet {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("Hej från /hello");
+
+
+//        String path = req.getPathInfo();
+////kolla om värdet finns och ta bort / från värdet
+//        String name = (path != null && path.length() > 1)
+//                ? path.substring(1)
+//                : "";
+////skriv ut värdet
+//        resp.getWriter().println("Hej " + name);
+
+
+        String name = req.getParameter("name");
+        resp.getWriter().println("Hej " + name);
+
+
+    }
 }
--- src/main/java/se/iths/johan/webbserver/RootServlet.java
+++ src/main/java/se/iths/johan/webbserver/RootServlet.java
@@ -1,4 +1,16 @@
 package se.iths.johan.webbserver;
 
-public class RootServlet {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class RootServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("Välkommen till startsidan");
+    }
 }
