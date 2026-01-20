0fbeda8033647703557a34bb7ac80712d3d372b7
johan <johan.persson@iths.se>
2026-01-20 14:56:02 +0100

    har skapat feature-a

--- src/main/java/se/iths/johan/webbserver/FeatureA.java
+++ src/main/java/se/iths/johan/webbserver/FeatureA.java
@@ -1,4 +1,19 @@
 package se.iths.johan.webbserver;
 
-public class FeatureA {
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/feature")
+public class FeatureA extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().println("Feature A");
+    }
+
 }
